5d74ec07f7beb623626101ee40c37e695e178576
Nikita <nktgzv@gmail.com>
2023-07-31 11:14:38 +0700

    pass mutable to funcs correctly

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ from calc_summary import calc_summary
 with open(DATASET_FILE_NAME, encoding='utf-8') as data_file:
     data = json.load(data_file)
 
-task_1_result = calc_tariff(data=data)
+task_1_result = calc_tariff(data=data[:])
 print('Task 1 result:', task_1_result)
 
-calc_summary(data=data, warehouses_tariffs=task_1_result)
+calc_summary(data=data[:], warehouses_tariffs=task_1_result.copy())
 print(f'Task 2 result is in {TASK_2_FILE_NAME}')
 
-calc_orders(data=data, warehouses_tariffs=task_1_result)
+calc_orders(data=data[:], warehouses_tariffs=task_1_result.copy())
 print(f'Task 3 result is in {TASK_3_FILE_NAME}')
 
-calc_income_percentage(data=data, warehouses_tariffs=task_1_result)
+calc_income_percentage(data=data[:], warehouses_tariffs=task_1_result.copy())
 print(f'Task 4 result is in {TASK_4_FILE_NAME}')
 
 
